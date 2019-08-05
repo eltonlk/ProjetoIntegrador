dbe3b481bb49130f7d213caf22fd171f166f0f4f
Elton Luis Kolling <eltonlk@gmail.com>
2019-08-04 23:37:26 -0300

    Alterado o código de retorno ao criar um usuário, categoria e material.

--- server-api/src/main/java/com/projeto/integrador/serverapi/controller/CategoriesController.java
+++ server-api/src/main/java/com/projeto/integrador/serverapi/controller/CategoriesController.java
@@ -5,6 +5,7 @@ import com.projeto.integrador.serverapi.repository.CategoriesRepository;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -46,6 +48,7 @@ public class CategoriesController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Category create(@RequestBody Category category) {
     return repository.save(category);
   }
--- server-api/src/main/java/com/projeto/integrador/serverapi/controller/UsersController.java
+++ server-api/src/main/java/com/projeto/integrador/serverapi/controller/UsersController.java
@@ -5,6 +5,7 @@ import com.projeto.integrador.serverapi.repository.UsersRepository;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -46,6 +48,7 @@ public class UsersController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public User create(@RequestBody User user) {
     return repository.save(user);
   }
