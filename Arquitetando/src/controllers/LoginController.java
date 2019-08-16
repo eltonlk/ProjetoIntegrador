199f096f7df0e6b1fcf1d3ca3b3bb7ae7f1940ff
Nicolas H Grave <nyko-23@hotmail.com>
2019-08-16 01:09:14 -0300

    Alterado layout da tela de login para centralizar os campos de login e adicionado mensagem de falha no login em toast.

--- Arquitetando/src/controllers/LoginController.java
+++ Arquitetando/src/controllers/LoginController.java
@@ -10,14 +10,20 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javax.swing.JOptionPane;
+import javafx.scene.layout.AnchorPane;
 import main.StageManager;
 import resources.AuthenticateUser;
+import resources.Snackbar;
 
 /**
  * FXML Controller class
@@ -25,13 +31,29 @@ import resources.AuthenticateUser;
  * @author nyko-
  */
 public class LoginController implements Initializable {
+
     @FXML
-    private JFXButton btnSubmit;
+    private AnchorPane root;
+    @FXML
+    private AnchorPane leftPane;
+    @FXML
+    private AnchorPane rightPane;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private AnchorPane loginPane;
+    @FXML
+    private Label title;
     @FXML
     private JFXTextField tfLogin;
     @FXML
     private JFXPasswordField tfPassword;
-   
+    @FXML
+    private JFXButton btnSubmit;
+
+    private final int LOGO_SIZE = 150;
+    private final int LOGIN_PANE_W_SIZE = 200;
+    private final int LOGIN_PANE_H_SIZE = 200;
 
     /**
      * Initializes the controller class.
@@ -41,7 +63,70 @@ public class LoginController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        tfLogin.setPromptText("Login");
+        tfPassword.setPromptText("Senha");
+        btnSubmit.setText("Entrar");
+        
+        addResizeListener();
+        setLogoPosition();
+        setLoginPanePosition();
 
+        title.setAlignment(Pos.CENTER);
+        tfLogin.requestFocus();
+        
+        root.setMinSize(600, 400);
+        leftPane.setMinSize(250, 400);
+        rightPane.setMinSize(350, 400);
+    }
+
+    private void addResizeListener() {
+        leftPane.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                setLogoPosition();
+            }
+        });
+
+        leftPane.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                setLogoPosition();
+            }
+        });
+        
+        rightPane.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                setLoginPanePosition();
+            }
+        });
+
+        rightPane.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                setLoginPanePosition();
+            }
+        });
+    }
+
+    private void setLoginPanePosition() {
+        double x = (rightPane.getWidth() - LOGIN_PANE_W_SIZE) / 2;
+        double y = (rightPane.getHeight() - LOGIN_PANE_H_SIZE) / 2;
+
+        AnchorPane.setRightAnchor(loginPane, x);
+        AnchorPane.setTopAnchor(loginPane, y);
+        AnchorPane.setLeftAnchor(loginPane, x);
+        AnchorPane.setBottomAnchor(loginPane, y);
+    }
+
+    private void setLogoPosition() {
+        double x = (leftPane.getWidth() - LOGO_SIZE) / 2;
+        double y = (leftPane.getHeight() - LOGO_SIZE) / 2;
+
+        AnchorPane.setRightAnchor(logo, x);
+        AnchorPane.setTopAnchor(logo, y);
+        AnchorPane.setLeftAnchor(logo, x);
+        AnchorPane.setBottomAnchor(logo, y);
     }
 
     @FXML
@@ -49,6 +134,7 @@ public class LoginController implements Initializable {
         submit();
     }
 
+    @FXML
     private void fieldKeyPressed(KeyEvent event) {
         if (event.getCode().equals(KeyCode.ENTER)) {
             submit();
@@ -63,8 +149,14 @@ public class LoginController implements Initializable {
             StageManager.setScene(StageManager.View.APPLICATION);
         } else {
             tfPassword.setText("");
-            tfPassword.requestFocus();
-            JOptionPane.showMessageDialog(null, "Usuário ou senha inválido!");
+            
+            new Snackbar("Usuário ou senha inválido!").show(rightPane);
+            
+            if (login.isEmpty()) {
+                tfLogin.requestFocus();
+            } else {
+                tfPassword.requestFocus();
+            }
         }
     }
 }
