e58311a6e0f936f0e2192d9dc75ec735c42f4d4b
Nicolas H Grave <nyko-23@hotmail.com>
2019-08-21 23:52:08 -0300

    Adicionado classe para fazer as conexções com o server e adicionado classe para adicionar os logs de erro em um arquivo para o ArquitetandoJavaSwing.

--- ArquitetandoJavaSwing/src/controllers/CategoriesController.java
+++ ArquitetandoJavaSwing/src/controllers/CategoriesController.java
@@ -6,17 +6,11 @@
 package controllers;
 
 import com.owlike.genson.GenericType;
-import com.owlike.genson.Genson;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import resources.ApiConnection;
+import resources.LogManager;
 import sources.Category;
 
 /**
@@ -24,142 +18,68 @@ import sources.Category;
  * @author nyko-
  */
 public class CategoriesController {
-
     public ArrayList<Category> list(String searchParams) {
         ArrayList<Category> categories = new ArrayList<>();
 
         try {
-            URL url = new URL("http://localhost:8080/categories");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            StringBuilder builder = new StringBuilder();
-
-            while ((output = br.readLine()) != null) {
-                builder.append(output);
-            }
-
-            System.out.println(builder.toString());
-
-            ArrayList<Category> categoriesObjects;
-            categoriesObjects = new Genson().deserialize(builder.toString(), new GenericType<ArrayList<Category>>(){});
+            ArrayList<Category> categoriesObjects = (ArrayList<Category>) new ApiConnection().getList("/categories", new GenericType<ArrayList<Category>>() {
+            });
 
             for (Category categoryObject : categoriesObjects) {
                 categoryObject.active();
                 categories.add(categoryObject);
             }
-
-            conn.disconnect();
         } catch (MalformedURLException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         }
 
         return categories;
     }
 
-    public String create(Category category) {
-        try {
-            URL url = new URL("http://localhost:8080/categories");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-
-            String input = "{\"name\":\"" + category.getName() + "\"}";
-
-            OutputStream os = conn.getOutputStream();
-            os.write(input.getBytes());
-            os.flush();
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            conn.disconnect();
-
-            return "";
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return "Não foi possivel gravar a Categoria.";
-    }
-
-    public Category find(int categoryId) {
-        Category category = new Category();
+    public Category find(int id) {
+        Category category = null;
 
         try {
-            URL url = new URL("http://localhost:8080/categories/" + categoryId);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            StringBuilder builder = new StringBuilder();
-
-            while ((output = br.readLine()) != null) {
-                builder.append(output);
-            }
-
-            System.out.println(builder.toString());
-
-            category = new Genson().deserialize(builder.toString(), Category.class);
-
+            category = (Category) new ApiConnection().get("/categories/" + id, Category.class);
             category.active();
-
-            conn.disconnect();
         } catch (MalformedURLException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         }
 
         return category;
     }
 
-    public String update(int id, Category category) {
+    public String create(Category category) {
         try {
-            URL url = new URL("http://localhost:8080/categories/" + id);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("PUT");
-            conn.setRequestProperty("Content-Type", "application/json");
-
             String input = "{\"name\":\"" + category.getName() + "\"}";
 
-            OutputStream os = conn.getOutputStream();
-            os.write(input.getBytes());
-            os.flush();
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            conn.disconnect();
+            new ApiConnection().post("/categories", input);
 
             return "";
         } catch (MalformedURLException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
+        }
+
+        return "Não foi possivel gravar a Categoria.";
+    }
+
+    public String update(int id, Category category) {
+        try {
+            String input = "{\"name\":\"" + category.getName() + "\"}";
+
+            new ApiConnection().put("/categories/" + id, input);
+
+            return "";
+        } catch (MalformedURLException ex) {
+            LogManager.doLog(ex.getMessage());
+        } catch (IOException ex) {
+            LogManager.doLog(ex.getMessage());
         }
 
         return "Não foi possivel gravar a categoria.";
@@ -167,26 +87,15 @@ public class CategoriesController {
 
     public String delete(int id) {
         try {
-            URL url = new URL("http://localhost:8080/categories/" + id);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("DELETE");
-            conn.setRequestProperty("Content-Type", "application/json");
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            conn.disconnect();
+            new ApiConnection().delete("/categories/" + id);
 
             return "";
         } catch (MalformedURLException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         } catch (IOException ex) {
-            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            LogManager.doLog(ex.getMessage());
         }
 
         return "Não foi possivel excluir a categoria";
     }
-
 }
