78bc1fb64ae0480c4eec2b6015487b7c67f2e4a0
Elton Luis Kolling <eltonlk@gmail.com>
2019-08-27 20:29:45 -0300

    Alterado o schema.sql.

--- server-api/src/main/resources/schema.sql
+++ server-api/src/main/resources/schema.sql
@@ -9,49 +9,57 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS roles;
 
 CREATE TABLE users (
-  id        SERIAL PRIMARY KEY,
-  name      varchar(255) NOT NULL,
-  email     varchar(255) NOT NULL,
-  username  varchar(255) NOT NULL,
-  password  varchar(255) NOT NULL,
-  active    boolean NOT NULL DEFAULT TRUE
+  id serial PRIMARY KEY,
+  name VARCHAR (255) UNIQUE NOT NULL,
+  email VARCHAR (255) UNIQUE NOT NULL,
+  username VARCHAR (255) UNIQUE NOT NULL,
+  password VARCHAR (255) NOT NULL,
+  active BOOLEAN
 );
 
-CREATE TABLE roles (
-  id   SERIAL PRIMARY KEY,
-  name varchar(255) NOT NULL
-);
+-- CREATE TABLE roles (
+--   id   SERIAL PRIMARY KEY,
+--   name varchar(255) NOT NULL
+-- );
 
-CREATE TABLE users_roles (
-  user_id INTEGER REFERENCES users(id),
-  role_id INTEGER REFERENCES roles(id)
-);
+-- CREATE TABLE users_roles (
+--   user_id INTEGER REFERENCES users(id),
+--   role_id INTEGER REFERENCES roles(id)
+-- );
 
-CREATE TABLE categories (
-  id     SERIAL PRIMARY KEY,
-  name   varchar(255) NOT NULL,
-  active boolean NOT NULL DEFAULT TRUE
-);
+-- CREATE TABLE categories (
+--   id     SERIAL PRIMARY KEY,
+--   name   varchar(255) NOT NULL,
+--   active boolean NOT NULL DEFAULT TRUE
+-- );
 
-CREATE TABLE materials (
-  id     SERIAL PRIMARY KEY,
-  name   varchar(255) NOT NULL,
-  active boolean NOT NULL DEFAULT TRUE
-);
+-- CREATE TABLE materials (
+--   id     SERIAL PRIMARY KEY,
+--   name   varchar(255) NOT NULL,
+--   active boolean NOT NULL DEFAULT TRUE
+-- );
+
+-- CREATE TABLE audits (
+--   id            SERIAL PRIMARY KEY,
+--   content       text NOT NULL,
+--   modified_by   varchar(255),
+--   modified_date timestamp,
+--   action        varchar(255) NOT NULL
+-- );
 
 CREATE TABLE audits (
-  id            SERIAL PRIMARY KEY,
-  content       text NOT NULL,
-  modified_by   varchar(255),
-  modified_date timestamp,
-  action        varchar(255) NOT NULL
+  id serial PRIMARY KEY,
+  content TEXT,
+  modified_by VARCHAR (255),
+  modified_date TIMESTAMP,
+  action VARCHAR (255) NOT NULL
 );
 
-CREATE TABLE options (
-  id    SERIAL PRIMARY KEY,
-  name  varchar(255),
-  value varchar(255)
-);
+-- CREATE TABLE options (
+--   id    SERIAL PRIMARY KEY,
+--   name  varchar(255),
+--   value varchar(255)
+-- );
 
 -- DROP TABLE IF EXISTS `projeto_integrador`.`audits`;
 -- DROP TABLE IF EXISTS `projeto_integrador`.`options`;
