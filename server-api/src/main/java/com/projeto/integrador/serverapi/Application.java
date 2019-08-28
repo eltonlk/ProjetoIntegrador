4b722c0425bb7f37a683ceb28f0c6fcb23eb2a85
Elton Luis Kolling <eltonlk@gmail.com>
2019-08-27 21:11:17 -0300

    Alteração.

--- server-api/src/main/java/com/projeto/integrador/serverapi/Application.java
+++ server-api/src/main/java/com/projeto/integrador/serverapi/Application.java
@@ -3,14 +3,18 @@ package com.projeto.integrador.serverapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableJpaRepositories
+@EntityScan
 @ComponentScan
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableWebSecurity
