25572a77bbda8d0ba6d1ff6dc457d4777ea5b263
Elton Luis Kolling <eltonlk@gmail.com>
2019-08-29 17:10:41 -0300

    Adicionado o campo thermalConductivityIndex nos materiais.

--- server-api/src/main/resources/schema.sql
+++ server-api/src/main/resources/schema.sql
@@ -36,7 +36,8 @@ CREATE TABLE categories (
 CREATE TABLE materials (
   id     SERIAL PRIMARY KEY,
   name   VARCHAR(255) NOT NULL,
-  active BOOLEAN
+  active BOOLEAN,
+  thermal_conductivity_index NUMERIC(5, 5) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE audits (
@@ -52,60 +53,3 @@ CREATE TABLE options (
   name  VARCHAR(255),
   value VARCHAR(255)
 );
-
--- DROP TABLE IF EXISTS `projeto_integrador`.`audits`;
--- DROP TABLE IF EXISTS `projeto_integrador`.`options`;
-
--- DROP TABLE IF EXISTS `projeto_integrador`.`categories`;
--- DROP TABLE IF EXISTS `projeto_integrador`.`materials`;
-
--- DROP TABLE IF EXISTS `projeto_integrador`.`users_roles`;
--- DROP TABLE IF EXISTS `projeto_integrador`.`users`;
--- DROP TABLE IF EXISTS `projeto_integrador`.`roles`;
-
--- CREATE TABLE `projeto_integrador`.`users` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `name` VARCHAR(255) NOT NULL,
---   `email` VARCHAR(255) NOT NULL,
---   `username` VARCHAR(255) NOT NULL,
---   `password` VARCHAR(255) NOT NULL,
---   `active` BOOLEAN DEFAULT 1
--- );
-
--- CREATE TABLE `projeto_integrador`.`roles` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `name` VARCHAR(255) NOT NULL
--- );
-
--- CREATE TABLE `projeto_integrador`.`users_roles` (
---   `user_id` INT NOT NULL,
---   `role_id` INT NOT NULL,
---   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE RESTRICT ON UPDATE CASCADE,
---   FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE RESTRICT ON UPDATE CASCADE
--- );
-
--- CREATE TABLE `projeto_integrador`.`categories` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `name` VARCHAR(255) NULL,
---   `active` BOOLEAN DEFAULT 1
--- );
-
--- CREATE TABLE `projeto_integrador`.`materials` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `name` VARCHAR(255) NOT NULL,
---   `active` BOOLEAN DEFAULT 1
--- );
-
--- CREATE TABLE `projeto_integrador`.`audits` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `content` TEXT NOT NULL,
---   `modified_by` VARCHAR(255) NULL,
---   `modified_date` DATETIME,
---   `action` VARCHAR(255) NOT NULL
--- );
-
--- CREATE TABLE `projeto_integrador`.`options` (
---   `id` INT PRIMARY KEY AUTO_INCREMENT,
---   `name` VARCHAR(255) NOT NULL,
---   `value` VARCHAR(255) NULL
--- );
