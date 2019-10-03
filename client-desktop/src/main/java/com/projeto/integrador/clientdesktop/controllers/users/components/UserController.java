593a0c43cbb968fdbaed1566eaa3c47394ffac5f
Elton Luis Kolling <eltonlk@gmail.com>
2019-10-03 09:38:06 -0300

    Definido para o ProjectController e UserController para sempre criar nova instancia.

--- client-desktop/src/main/java/com/projeto/integrador/clientdesktop/controllers/users/components/UserController.java
+++ client-desktop/src/main/java/com/projeto/integrador/clientdesktop/controllers/users/components/UserController.java
@@ -17,10 +17,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class UserController implements Initializable {
 
   @Lazy
