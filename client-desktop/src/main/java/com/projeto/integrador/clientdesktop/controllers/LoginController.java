cb30232956d29aa60d35101dfe39084ed2e3d5d8
Nicolas Grave <nyko-23@hotmail.com>
2019-10-15 22:45:47 -0300

    Adicionado grid nos formulário e alterado layout do show do projeto.

--- client-desktop/src/main/java/com/projeto/integrador/clientdesktop/controllers/LoginController.java
+++ client-desktop/src/main/java/com/projeto/integrador/clientdesktop/controllers/LoginController.java
@@ -39,6 +39,7 @@ public class LoginController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    // TODO: remove on production
     usernameInput.setText("admin");
     passwordInput.setText("123456789");
   }
