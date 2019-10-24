bfe1205bc28aa3458b81135e91f70dcbdb6df98c
Nicolas <nyko-23@hotmail.com>
2019-10-24 10:35:51 -0300

    Ao alterar os dados de temperatura ou radiação do projeto ou orientação da face deve refazer os calculos dos componentes no server-api-rails.

--- server-api-rails/app/models/face.rb
+++ server-api-rails/app/models/face.rb
@@ -11,11 +11,18 @@ class Face < ApplicationRecord
   validates :orientation, inclusion: { in: %w(north north_east north_west south south_east south_west east west)  }
 
   before_save :set_heat_flow
+  after_update :save_components
   after_save :save_room
 
   private
     def set_heat_flow
       self.heat_flow = components.sum(:heat_flow)
+    end
+
+    def save_components
+      if orientation_previously_changed?
+        components.each(&:save)
+      end
     end
 
     def save_room
