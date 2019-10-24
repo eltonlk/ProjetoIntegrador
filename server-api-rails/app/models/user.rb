24a4b1f5bf53ef6e69dc5201e0ff42949cd562d2
Elton Luis Kolling <eltonlk@gmail.com>
2019-10-24 14:41:31 -0300

    Retirado a validação de senha ao atualizar um usuário e ordenar pelo nome por padrão.

--- server-api-rails/app/models/user.rb
+++ server-api-rails/app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   audited
 
-  has_secure_password
+  has_secure_password validations: false
 
   has_many :roles, class_name: 'UserRole', dependent: :destroy
 
   validates :email   , presence: true, uniqueness: true
   validates :name    , presence: true
-  validates :password, presence: true
+  validates :password, presence: true, on: :create
   validates :username, presence: true
+
+  default_scope -> { order :name }
 end
