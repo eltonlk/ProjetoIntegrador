ca32d0713e66646da5e6ccac13ecbb39b632646d
Elton Luis Kolling <eltonlk@gmail.com>
2019-11-07 19:48:28 -0300

    Melhorias no pdf do projeto.

--- server-api-rails/app/pdfs/project_pdf.rb
+++ server-api-rails/app/pdfs/project_pdf.rb
@@ -10,16 +10,29 @@ class ProjectPdf
   end
 
   def content
-    text "Projeto: #{project.name}"
-    text "Estação: #{project.season}"
-    text "Temperatura Externa: #{project.external_temperature}"
-    text "Temperatura Interna: #{project.internal_temperature}"
-    text "Radiação Solar: #{project.solar_radiation.name}"
+    text "Projeto", align: :center
 
-    text "Cômodos:"
+    move_down 5
+
+    text project.name, align: :center, size: 32
+
+    move_down 20
+
+    text "Estação: <b>#{project.season}</b>"
+    move_down 5
+    text "Temperatura Externa: <b>#{project.external_temperature}</b>"
+    move_down 5
+    text "Temperatura Interna: <b>#{project.internal_temperature}</b>"
+    move_down 5
+    text "Radiação Solar: <b>#{project.solar_radiation.name}</b>"
+
+    move_down 20
+
+    text "<b>Cômodos:</b>"
 
     project.rooms.each do |room|
       text "#{room.name} - carga de calor: #{room.heat_load}"
+      move_down 5
     end
   end
 end
