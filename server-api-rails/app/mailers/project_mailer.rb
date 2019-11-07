4dbf3e26ee4e3b8406fa911c7660a537b9793bce
Elton Luis Kolling <eltonlk@gmail.com>
2019-11-07 19:35:14 -0300

    Gerar um pdf do projeto para enviar com o e-mail.

--- server-api-rails/app/mailers/project_mailer.rb
+++ server-api-rails/app/mailers/project_mailer.rb
@@ -3,6 +3,13 @@ class ProjectMailer < ApplicationMailer
   def send_mail project, email
     @project = project
 
+    pdf = ProjectPdf.new @project
+
+    attachments['Projeto.pdf'] = {
+      mime_type: 'application/pdf',
+      content: project.render
+    }
+
     mail to: email, subject: "Dados do Projeto #{@project.name}"
   end
 
---
+++ server-api-rails/app/pdfs/project_pdf.rb
@@ -0,0 +1,26 @@
+class ProjectPdf
+  include Prawn::View
+
+  attr_accessor :project
+
+  def initialize project
+    @project = project
+
+    content
+  end
+
+  def content
+    text "Hello World!"
+    text "Projeto: #{project.name}"
+    text "Estação: #{project.season}"
+    text "Temperatura Externa: #{project.external_temperature}"
+    text "Temperatura Interna: #{project.internal_temperature}"
+    text "Radiação Solar: #{project.solar_radiation.name}"
+
+    text "Cômodos:"
+
+    project.rooms.each do |room|
+      text "#{room.name} - carga de calor: #{room.heat_load}"
+    end
+  end
+end
